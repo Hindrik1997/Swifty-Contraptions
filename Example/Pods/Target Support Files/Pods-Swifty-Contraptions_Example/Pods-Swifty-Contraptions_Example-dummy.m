786b12c34c1fe3c303f7b074e55a1c2e47539c6b
Hindrik Stegenga <hindrik@Hindriks-Mac-Pro.local>
2017-09-19 19:49:24 +0200

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-Swifty-Contraptions_Example/Pods-Swifty-Contraptions_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swifty_Contraptions_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Swifty_Contraptions_Example
+@end
---
+++ Example/Pods/Target Support Files/Swifty-Contraptions/Swifty-Contraptions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Swifty_Contraptions : NSObject
+@end
+@implementation PodsDummy_Swifty_Contraptions
+@end
